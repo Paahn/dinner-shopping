3a23cb6ff017a4c23f01cac1fe2a4e1afca0f2be
Paahn <paahndev@gmail.com>
2021-07-21 20:56:45 -0700

    created event emitter

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
     public collapsed = true;
+    public featureSelected = new EventEmitter<string>();
+
+    public onSelect(feature: string) {
+        this.featureSelected.emit(feature);
+    }
     
 }
