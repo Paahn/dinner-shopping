0817386698d4b88067b6cdc55816111e38dd5a20
Paahn <paahndev@gmail.com>
2021-07-30 23:03:38 -0700

    access modifiers

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -7,14 +7,14 @@ import { Ingredient } from 'src/app/models/ingredient.model';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-  ingredients: Ingredient[] = [
+  public ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 3)
   ];
 
   constructor() { }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
   }
 
 }
