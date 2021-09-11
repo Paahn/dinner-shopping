421b0d8aed71032980942132455e993c742d0eef
Paahn <paahndev@gmail.com>
2021-09-11 11:30:02 -0700

    added ingredient to recipe
    updated recipes to include ingredients with mock data

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,11 +1,15 @@
+import { Ingredient } from "./ingredient.model";
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[];
 
-    constructor(name: string, descr: string, imageP: string) {
+    constructor(name: string, descr: string, imageP: string, ingredients: Ingredient[]) {
         this.name = name;
         this.description = descr;
         this.imagePath = imageP;
+        this.ingredients = ingredients;
     }
 }
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../models/ingredient.model";
 import { Recipe } from "../models/recipe.model";
 
 export class RecipeService {
@@ -6,14 +7,24 @@ export class RecipeService {
     
     private recipes: Recipe[] = [
         new Recipe(
-        'Test recipe',
-        'First recipe to test this out',
-        'https://static.onecms.io/wp-content/uploads/sites/9/2018/03/recipetester-ft-0318.jpg'
+        'Tzatziki',
+        'Homemade tzatziki',
+        'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2014%2F02%2F21%2F2014-r-xl-cucumber-dill-tzatziki.jpg',
+        [
+            new Ingredient('Greek Yogurt', 1),
+            new Ingredient('Cucumber', 1),
+            new Ingredient('Dill weed', 1)
+        ]
         ),
         new Recipe(
-          'A second recipe',
-          'Second recipe to test this out',
-          'https://static.onecms.io/wp-content/uploads/sites/9/2018/03/recipetester-ft-0318.jpg'
+          'Souvlaki',
+          'Delicious chicken skewers',
+          'https://www.mygreekdish.com/wp-content/uploads/2013/05/Greek-Chicken-Souvlaki-Skewers-recipe-5-1536x1111.jpg',
+          [
+              new Ingredient('Chicken Breast', 2),
+              new Ingredient('Lemon', 1),
+              new Ingredient('Oregano', 1)
+          ]
         )
     ];
 
