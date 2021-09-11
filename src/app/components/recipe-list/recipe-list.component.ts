d24809c30bd2ea2ec3c1ffbb861ee510309fefc2
Paahn <paahndev@gmail.com>
2021-09-10 21:26:09 -0700

    injected recipe service into RecipeList to get our recipes there

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'], 
+  providers: [RecipeService]
 })
 export class RecipeListComponent implements OnInit {
   @Output() public recipeIsSelected = new EventEmitter<Recipe>();
+  public recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   public ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   public onRecipeSelected(recipe: Recipe): void {
