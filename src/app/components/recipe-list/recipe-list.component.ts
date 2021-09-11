23c17fda0d87d203b0967886bc0f8eac6b641e81
Paahn <paahndev@gmail.com>
2021-09-10 21:03:14 -0700

    moved recipes array into service
    made array private so we cant access it from outside without a method

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -8,18 +8,6 @@ import { Recipe } from 'src/app/models/recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() public recipeIsSelected = new EventEmitter<Recipe>();
-  public recipes: Recipe[] = [
-    new Recipe(
-    'Test recipe',
-    'First recipe to test this out',
-    'https://static.onecms.io/wp-content/uploads/sites/9/2018/03/recipetester-ft-0318.jpg'
-    ),
-    new Recipe(
-      'A second recipe',
-      'Second recipe to test this out',
-      'https://static.onecms.io/wp-content/uploads/sites/9/2018/03/recipetester-ft-0318.jpg'
-      )
-  ];
 
   constructor() { }
 
--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,3 +1,17 @@
+import { Recipe } from "../models/recipe.model";
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe(
+        'Test recipe',
+        'First recipe to test this out',
+        'https://static.onecms.io/wp-content/uploads/sites/9/2018/03/recipetester-ft-0318.jpg'
+        ),
+        new Recipe(
+          'A second recipe',
+          'Second recipe to test this out',
+          'https://static.onecms.io/wp-content/uploads/sites/9/2018/03/recipetester-ft-0318.jpg'
+        )
+    ];
+
 }
