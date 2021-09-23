ddfdb2fe349f7afe33f2ad61bdf50d374a156203
Panos <paahndev@gmail.com>
2021-09-22 18:29:15 -0700

    navigate to /new when clicking on New Recipe button
    to be able to use relative routing in our navigate method, in this case 'new', we need to inform our router of the current route, which is why we inject route:ActivatedRoute and use the relativeTo syntax

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from 'src/app/models/recipe.model';
 import { RecipeService } from 'src/app/services/recipe.service';
 
@@ -10,10 +11,18 @@ import { RecipeService } from 'src/app/services/recipe.service';
 export class RecipeListComponent implements OnInit {
   public recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) { }
 
   public ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
+  }
+
+  public onNewRecipe(): void {
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
 }
