09b92ebcb6fb2778dbded472128ded99651c38e1
Panos <paahndev@gmail.com>
2021-10-04 22:24:24 -0700

    added ingredients to reactive form

--- src/app/components/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipe-edit/recipe-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+// import { timeStamp } from 'console';
 import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
@@ -21,6 +22,10 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  public get controls(): AbstractControl[] {
+    return (<FormArray>this.recipeForm.get('ingredients')).controls;
+  }
+
   public ngOnInit(): void {
     this.route.params
       .subscribe(
@@ -36,17 +41,29 @@ export class RecipeEditComponent implements OnInit {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
+    let recipeIngredients = new FormArray([]);
 
     if (this.editMode) {
       const recipe = this.recipeService.getRecipe(this.id);
       recipeName = recipe.name;
       recipeImagePath = recipe.imagePath;
       recipeDescription = recipe.description;
+      if (recipe['ingredients']) {
+        for (let ingredient of recipe.ingredients) {
+          recipeIngredients.push(
+            new FormGroup({
+              'name': new FormControl(ingredient.name),
+              'amount': new FormControl(ingredient.amount)
+            })
+          )
+        }
+      }
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName),
       'imagePath': new FormControl(recipeImagePath),
-      'description': new FormControl(recipeDescription)
+      'description': new FormControl(recipeDescription),
+      'ingredients': recipeIngredients
     });
 
   }
