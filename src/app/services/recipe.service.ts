ae15adf6ed9a17c8799fac119dbcea5a80933a41
Panos <paahndev@gmail.com>
2021-10-28 23:02:45 -0700

    initializing recipes as empty array

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -8,28 +8,7 @@ import { ShoppingListService } from "./shopping-list.service";
 export class RecipeService {
   public recipeListModified = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-      new Recipe(
-      'Tzatziki',
-      'Homemade tzatziki',
-      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F9%2F2014%2F02%2F21%2F2014-r-xl-cucumber-dill-tzatziki.jpg',
-      [
-          new Ingredient('Greek Yogurt', 1),
-          new Ingredient('Cucumber', 1),
-          new Ingredient('Dill weed', 1)
-      ]
-      ),
-      new Recipe(
-        'Souvlaki',
-        'Delicious chicken skewers',
-        'https://www.mygreekdish.com/wp-content/uploads/2013/05/Greek-Chicken-Souvlaki-Skewers-recipe-5-1536x1111.jpg',
-        [
-            new Ingredient('Chicken Breast', 2),
-            new Ingredient('Lemon', 1),
-            new Ingredient('Oregano', 1)
-        ]
-      )
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
 
