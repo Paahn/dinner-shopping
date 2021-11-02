be71ede3953bb4ddca370a06bab02a1da818d4e0
Panos <paahndev@gmail.com>
2021-11-01 21:22:33 -0700

    fix bug where editing a recipe would get overwritten by API call data

--- src/app/services/recipe-resolver.service.ts
+++ src/app/services/recipe-resolver.service.ts
@@ -2,11 +2,21 @@ import { Injectable } from "@angular/core";
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "../models/recipe.model";
 import { RecipeResource } from "./recipe-resource.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({ providedIn: 'root'})
 export class RecipesResolverService implements Resolve<Recipe[]> {
-  constructor(private recipeResource: RecipeResource) {}
+  constructor(
+    private recipeResource: RecipeResource,
+    private recipeServoce: RecipeService
+  ) {}
+
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.recipeResource.getRecipes();
+    const recipes = this.recipeServoce.getRecipes();
+    if (recipes.length === 0) {
+      return this.recipeResource.getRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
