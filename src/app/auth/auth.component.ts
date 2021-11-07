e3642133d6cc9704e1599adcb4d96abbf72be329
Paahn <paahndev@gmail.com>
2021-11-07 11:57:12 -0800

    implement signup

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,14 +10,32 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent implements OnInit {
   public isLoginMode: boolean = true;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   public onSwitchAuthMode() {
     this.isLoginMode = !this.isLoginMode;
   }
 
   public onSubmit(form: NgForm): void {
-    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      // todo implement login
+    } else {
+      this.authService.signUp(email, password)
+        .subscribe(
+          responseData => {
+            console.log(responseData);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+    }
     form.reset();
   }
 
