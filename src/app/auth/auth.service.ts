8c24dde0ddbc57b41bf54454a63cf09e5aac92cb
Paahn <paahndev@gmail.com>
2021-11-07 11:49:19 -0800

    interface with response data from auth signup API

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,6 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from '../../environments/environment.custom';
 
+interface AuthResponseData {
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+}
+
 @Injectable({ providedIn:'root' })
 export class AuthService {
     constructor(private http: HttpClient) {}
