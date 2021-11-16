6dc654fe4368dbd22c758dba26f32f25a9bf4700
Panos <paahndev@gmail.com>
2021-11-15 20:17:44 -0800

    change the UI based on if user is authenticated/signed in

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../auth/auth.service';
 import { RecipeResource } from '../services/recipe-resource.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSubscription: Subscription;
+  public isAuthenticated: boolean = false;
+
   constructor(
-    private recipeResource: RecipeResource
+    private recipeResource: RecipeResource,
+    private authService: AuthService
   ) {}
+
+  public ngOnInit(): void {
+    this.userSubscription = this.authService.user.subscribe( user => {
+      this.isAuthenticated = !user ? false : true;
+    });
+  }
 
   public onCreateRecipe(): void {
     this.recipeResource.createRecipes();
@@ -16,5 +28,9 @@ export class HeaderComponent {
 
   public onGetRecipes(): void {
     this.recipeResource.getRecipes().subscribe();
+  }
+
+  public ngOnDestroy(): void {
+    this.userSubscription.unsubscribe();
   }
 }
