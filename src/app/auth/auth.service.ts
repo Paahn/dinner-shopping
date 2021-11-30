fa2a6a867c9253caee74738db133f2c18580bee1
Panos <paahndev@gmail.com>
2021-11-29 22:18:25 -0800

    storing user object in local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -82,6 +82,7 @@ export class AuthService {
       expirationDate
     );
     this.user.next(user);
+    localStorage.setItem('userData', JSON.stringify(user));
   }
 
   private handleError(error: HttpErrorResponse) {
