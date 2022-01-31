e812fa7322c24a8a375a04043d21b002c2f3f754
Panos <paahndev@gmail.com>
2022-01-30 16:23:41 -0800

    unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListService } from './modules/shopping-list/services/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeService } from './modules/recipes/services/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { RecipesModule } from './modules/recipes/recipes.module';
 import { ShoppingListModule } from './modules/shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
